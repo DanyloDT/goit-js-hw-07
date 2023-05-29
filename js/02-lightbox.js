fd7eb95c2172f06e870f2e6b615db8c73c665fae
Danylo Diachenko <danylodiachenko@gmail.com>
2023-05-29 21:39:56 +0300

    правки

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,7 +9,7 @@ const makeItemCard = ({preview, original, description} = {}) => {
     return `
     <li class="gallery__item">
         <a class="gallery__link" href="${original}">
-            <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+            <img class="gallery__image" src="${preview}" alt="${description}"/>
         </a>
     </li>`;
 };
